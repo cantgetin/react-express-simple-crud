8774f08783d2d938363c4525d2b9ebc729e3f5ef
cantgetin <84032933+cantgetin@users.noreply.github.com>
2023-01-22 17:54:21 +0500

    Client optimizations

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,63 +1,32 @@
 import {useEffect, useState} from 'react'
 import axios from 'axios'
-import Modal from "./components/UI/Modal";
+import Modal from "./components/UI/Modal/Modal";
 import {Formik, Form, Field, ErrorMessage} from 'formik';
 import Button from "./components/UI/Button";
-
-interface User {
-    id: number,
-    firstName: string,
-    lastName: string,
-    age: number
-}
-
-const ModalUser: Omit<User, "id"> = {
-    age: 0, firstName: "", lastName: ""
-}
+import {User} from "./types/types"
 
 function App() {
 
     const [users, setUsers] = useState<User[]>([]);
     const [selectedUser, setSelectedUser] = useState<User | undefined>(undefined)
-
-    const fetchUsers = () => axios.get('http://localhost:8005/api/read')
-
+    const fetchUsers = () => axios.get('http://localhost:8005/api/read').then(r => setUsers(r.data));
 
     const [createModalActive, setCreateModalActive] = useState<boolean>(false)
     const [deleteModalActive, setDeleteModalActive] = useState<boolean>(false)
     const [updateModalActive, setUpdateModalActive] = useState<boolean>(false)
 
     useEffect(() => {
-        fetchUsers().then(r => setUsers(r.data));
+        void fetchUsers()
     }, [])
-
-    const userAdded = () => {
-        fetchUsers().then(r => setUsers(r.data));
+    const userListUpdated = () => {
+        void fetchUsers()
+        setUpdateModalActive(false)
+        setDeleteModalActive(false)
         setCreateModalActive(false)
     }
 
-    const userUpdated = () => {
-        fetchUsers().then(r => setUsers(r.data));
-        setUpdateModalActive(false)
-    }
-
-    const openDeleteUserModal = (id: number) => {
-        setSelectedUser(users.find(u => u.id == id))
-        setDeleteModalActive(true)
-    }
-
-    const openUpdateUserModal = (id: number) => {
-        setSelectedUser(users.find(u => u.id == id))
-        setUpdateModalActive(true)
-    }
-
-    const deleteUser = (id: number) => {
-        setDeleteModalActive(false)
-        axios.get(`http://localhost:8005/api/delete/${id}`).then(r =>
-            r.status == 200 ?
-                fetchUsers().then(r => setUsers(r.data))
-                : null
-        )
+    const ModalUser: Omit<User, "id"> = {
+        age: 0, firstName: "", lastName: ""
     }
 
     return (
@@ -72,10 +41,10 @@ function App() {
                             {users.map(u =>
                                 <div className="h-64 w-64 rounded-md shadow-md bg-blue-200 overflow-hidden" key={u.id}>
                                     <div className="bg-white h-3/4"></div>
-                                    <div className="text-center px-2">
+                                    <div className="text-center px-2" onMouseEnter={() => setSelectedUser(u)}>
                                         <div>{u.firstName} {u.lastName} age: {u.age}</div>
-                                        <Button onClick={() => openUpdateUserModal(u.id)}>Update</Button>
-                                        <Button onClick={() => openDeleteUserModal(u.id)}>Delete</Button>
+                                        <Button onClick={() => setUpdateModalActive(true)}>Update</Button>
+                                        <Button onClick={() => setDeleteModalActive(true)}>Delete</Button>
                                     </div>
                                 </div>
                             )}
@@ -97,12 +66,14 @@ function App() {
                     //     if (!value.surname) errors.surname = 'Required';
                     //     return errors;
                     // }}
-                    onSubmit={(values, {setSubmitting}) => {
+                    onSubmit={(values, {setSubmitting, resetForm}) => {
                         axios.post('http://localhost:8005/api/create', {...values}).then(r => {
                             r.status == 200
-                                ? userAdded()
+                                ? userListUpdated()
                                 : null
                         })
+                        resetForm()
+                        setSubmitting(false)
                     }}
                 >
                     {({isSubmitting}) => (
@@ -128,7 +99,13 @@ function App() {
                 <div className="flex flex-col gap-5">
                     Are you sure you want to delete {selectedUser?.firstName} {selectedUser?.lastName}?
                     <div className="flex gap-5 w-full justify-center">
-                        <Button onClick={() => deleteUser(selectedUser!.id)}>Yes</Button>
+                        <Button onClick={() => {
+                            axios.get(`http://localhost:8005/api/delete/${selectedUser!.id}`).then(r =>
+                                r.status == 200 ?
+                                    userListUpdated()
+                                    : null
+                            )
+                        }}>Yes</Button>
                         <Button onClick={() => setDeleteModalActive(false)}>No</Button>
                     </div>
                 </div>
@@ -138,7 +115,8 @@ function App() {
                     Update {selectedUser?.firstName} {selectedUser?.lastName}?
 
                     <Formik
-                        initialValues={ModalUser}
+                        initialValues={selectedUser!}
+                        enableReinitialize={true}
                         // validate={values => {
                         //     const errors = {age: ''};
                         //     if (!values.name) errors.email = 'Required';
@@ -148,7 +126,7 @@ function App() {
                         onSubmit={(values, {setSubmitting}) => {
                             axios.post(`http://localhost:8005/api/update/${selectedUser!.id}`, {...values}).then(r => {
                                 r.status == 200
-                                    ? userUpdated()
+                                    ? userListUpdated()
                                     : null
                             })
                         }}
@@ -171,8 +149,6 @@ function App() {
                             </Form>
                         )}
                     </Formik>
-
-
                 </div>
             </Modal>
         </div>
